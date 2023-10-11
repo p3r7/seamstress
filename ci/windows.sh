23a929028416e181d40ae55f4dbe49c400449e05
p3r7 <11557146+p3r7@users.noreply.github.com>
2023-10-11 20:59:43 +0200

    who's a big dum-dum

--- ci/windows.sh
+++ ci/windows.sh
@@ -5,9 +5,9 @@ mkdir /opt
 
 # install zig
 mkdir /opt/zig
-wget https://ziglang.org/builds/zig-linux-x86_64-0.11.0.tar.xz 1> /dev/null
-tar -xf zig-linux-x86_64-0.11.0.tar.xz -C /tmp
-cp -R /tmp/zig-linux-x86_64-0.11.0/* /opt/zig/
+wget https://ziglang.org/builds/zig-windows-x86_64-0.11.0.tar.xz 1> /dev/null
+tar -xf zig-windows-x86_64-0.11.0.tar.xz -C /tmp
+cp -R /tmp/zig-windows-x86_64-0.11.0/* /opt/zig
 
 # fix liblo missing alias
 ln -s /mingw64/bin/liblo-7.dll /mingw64/bin/lo.dll
