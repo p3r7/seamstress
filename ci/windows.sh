7c658d6cf69582f735328834ebff3f58c39062dd
p3r7 <11557146+p3r7@users.noreply.github.com>
2023-10-11 20:57:41 +0200

    mhm, surprises arise

--- ci/windows.sh
+++ ci/windows.sh
@@ -7,10 +7,11 @@ mkdir /opt
 mkdir /opt/zig
 wget https://ziglang.org/builds/zig-linux-x86_64-0.11.0.tar.xz 1> /dev/null
 tar -xf zig-linux-x86_64-0.11.0.tar.xz -C /tmp
-cp -R /tmp/zig-linux-x86_64-0.11.0/* /opt/zig
+cp -R /tmp/zig-linux-x86_64-0.11.0/* /opt/zig/
 
 # fix liblo missing alias
 ln -s /mingw64/bin/liblo-7.dll /mingw64/bin/lo.dll
 
 # build
-/opt/zig/zig build -Doptimize=ReleaseFast -Dcpu=x86_64 --summary all
+ls /opt/zig/
+# /opt/zig/zig build -Doptimize=ReleaseFast -Dcpu=x86_64 --summary all
