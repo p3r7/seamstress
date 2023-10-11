179704acf05ffa5690c8a39af83d8c8f81d5e64f
p3r7 <11557146+p3r7@users.noreply.github.com>
2023-10-11 21:12:36 +0200

    is a zip on windows

--- ci/windows.sh
+++ ci/windows.sh
@@ -5,9 +5,9 @@ mkdir /opt
 
 # install zig
 mkdir /opt/zig
-wget https://ziglang.org/builds/zig-windows-x86_64-0.11.0.tar.xz 1> /dev/null
-tar -xf zig-windows-x86_64-0.11.0.tar.xz -C /tmp
-cp -R /tmp/zig-windows-x86_64-0.11.0/* /opt/zig
+wget https://ziglang.org/builds/zig-windows-x86_64-0.11.0.zip 1> /dev/null
+unzip zig-windows-x86_64-0.11.0.zip
+cp -R zig-windows-x86_64-0.11.0/* /opt/zig
 
 # fix liblo missing alias
 ln -s /mingw64/bin/liblo-7.dll /mingw64/bin/lo.dll
