52222769b2c73e034c083b51f7861e337cb106f1
p3r7 <11557146+p3r7@users.noreply.github.com>
2023-10-11 18:32:58 +0200

    let's try this

---
+++ ci/windows.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir /tmp
+mkdir /opt
+
+# install zig
+mkdir /opt/zig
+wget https://ziglang.org/builds/zig-linux-x86_64-0.11.0.tar.xz
+tar -xf zig-linux-x86_64-0.11.0.tar.xz -C /tmp
+cp -R /tmp/zig-linux-x86_64-0.11.0/* /opt/zig
+
+# fix liblo missing alias
+ln -s /mingw64/bin/liblo-7.dll /mingw64/bin/lo.dll
+
+# build
+/opt/zig/zig build -Doptimize=ReleaseFast -Dcpu=x86_64 --summary all
