10a20dfc3621458b808ce4c441da726a6d03e387
p3r7 <11557146+p3r7@users.noreply.github.com>
2023-10-11 21:30:37 +0200

    less verbose

--- ci/windows.sh
+++ ci/windows.sh
@@ -6,7 +6,7 @@ mkdir /opt
 # install zig
 mkdir /opt/zig
 wget --no-verbose https://ziglang.org/builds/zig-windows-x86_64-0.11.0.zip
-unzip zig-windows-x86_64-0.11.0.zip
+unzip -qq zig-windows-x86_64-0.11.0.zip
 cp -R zig-windows-x86_64-0.11.0/* /opt/zig
 
 # fix liblo missing alias
